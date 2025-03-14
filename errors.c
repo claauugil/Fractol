3781f198888a480dbdc99302caed21d433a81711
Claudia <sofiagil2288@gmail.com>
2025-03-14 11:56:31 +0100

    error de input solucionado

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: claudia <claudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:48:16 by claudia           #+#    #+#             */
-/*   Updated: 2025/01/15 18:34:19 by claudia          ###   ########.fr       */
+/*   Updated: 2025/03/14 11:55:26 by claudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	print_error(void)
 {
-	write(2, "check correct arguments to run the program", 43);
+	write(2, "check correct arguments to run the program\n", 44);
 	exit(EXIT_FAILURE);
 }
 
 void	malloc_error(void)
 {
-	perror("Error while allocating memory with malloc");
+	perror("Error while allocating memory with malloc\n");
 	exit(EXIT_FAILURE);
 }
